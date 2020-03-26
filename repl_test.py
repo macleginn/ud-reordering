10fa0d5a2ecf39c30e3cc285b31da8f85ac70762
Dmitry Nikolaev <dsnikolaev@gmail.com>
2020-03-26 20:07:43 +0200

    Update the repl_test

--- repl_test.py
+++ repl_test.py
@@ -17,7 +17,8 @@ def conllu2trees(path):
 # Learn Japanese ordering and apply it to English.
 data_dir = pathlib.Path('data')
 trees_ja = conllu2trees(data_dir / 'ja_pud-ud-test.conllu')
-trees_en = conllu2trees(data_dir / 'en_ewt-ud-train.conllu')
+trees_en_ewt = conllu2trees(data_dir / 'en_ewt-ud-train.conllu')
+trees_en_pud = conllu2trees(data_dir / 'en_pud-ud-test.conllu')
 
 importlib.reload(U)
 importlib.reload(E)
@@ -26,12 +27,18 @@ estimates_ja = E.get_ml_directionality_estimates(trees_ja)
 assert E.ordering_is_valid(estimates_ja)
 
 importlib.reload(R)
-reordered = []
-for tree in trees_en:
-    try:
-        reordered.append(R.reorder(tree, estimates_ja))
-    except ValueError:
-        pprint(tree.id_lines[-1])
-        continue
 
+for i, treebank in enumerate([
+    trees_en_pud,
+    trees_en_ewt
+]):
+    reordered = []
+    for tree in treebank:
+        try:
+            reordered.append(R.reorder(tree, estimates_ja))
+        except ValueError:
+            pprint(tree.id_lines[-1])
+            continue
+    with open(data_dir / f'en_{["pud", "ewt"][i]}_reordered.conllu', 'w') as out:
+        out.write('\n\n'.join(str(t) for t in reordered))
 
